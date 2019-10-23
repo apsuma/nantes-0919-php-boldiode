23d3a95dadc72ef46f5e55889e75972f526c8458
Armoredbrain <l1ght3rth4nl1ght@gmail.com>
2019-10-23 17:17:03 +0200

    add method for RoomManager and RoomController, creation _addForm

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -6,6 +6,9 @@
 namespace App\Controller;
 
 use App\Model\RoomManager;
+use App\Model\ViewManager;
+use App\Model\PriceManager;
+use App\Model\ThemeManager;
 
 /**
  * Class RoomController
@@ -15,22 +18,23 @@ class RoomController extends AbstractController
 {
     public function add()
     {
+        $viewManager = new ViewManager();
+        $views = $viewManager->selectAll();
+        $priceManager = new PriceManager();
+        $prices = $priceManager->selectAll();
+        $themeManager = new ThemeManager();
+        $themes = $themeManager->selectAll();
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $roomManager = new RoomManager();
-            $room = [
-                'name' => $_POST['name'],
-                'description' => $_POST['description'],
-                'nb_bed' => $_POST['nb_bed'],
-                'surface' => $_POST['surface'],
-                'id_price' => $_POST['id_price'],
-                'id_view' => $_POST['id_view'],
-                'id_theme' => $_POST['id_theme'],
-            ];
-            $id = $roomManager->insert($room);
+            $id = $roomManager->insert($_POST);
             header('Location:/room/show/' . $id);
         }
 
-        return $this->twig->render('Room/add.html.twig');
+        return $this->twig->render('Room/add.html.twig', [
+            'views' => $views,
+            'prices' => $prices,
+            'themes' => $themes
+        ]);
     }
 }
