17c9008a35018acc7694347061ead1e594d1fa7f
Armoredbrain <l1ght3rth4nl1ght@gmail.com>
2019-10-23 15:58:43 +0200

    add method for RoomManager and RoomController, creation _addForm

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -13,5 +13,24 @@ use App\Model\RoomManager;
  */
 class RoomController extends AbstractController
 {
+    public function add()
+    {
 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $roomManager = new RoomManager();
+            $room = [
+                'name' => $_POST['name'],
+                'description' => $_POST['description'],
+                'nb_bed' => $_POST['nb_bed'],
+                'surface' => $_POST['surface'],
+                'id_price' => $_POST['id_price'],
+                'id_view' => $_POST['id_view'],
+                'id_theme' => $_POST['id_theme'],
+            ];
+            $id = $roomManager->insert($room);
+            header('Location:/room/show/' . $id);
+        }
+
+        return $this->twig->render('Room/add.html.twig');
+    }
 }
--- src/Model/RoomManager.php
+++ src/Model/RoomManager.php
@@ -20,4 +20,27 @@ class RoomManager extends AbstractManager
     {
         parent::__construct(self::TABLE);
     }
+
+    /**
+     * @param array $room
+     * @return int
+     */
+    public function insert(array $room): int
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("INSERT INTO ". self::TABLE .
+            "(name,description,nb_bed,surface,id_price,id_view,id_theme) 
+            VALUES (:name,:description,:nb_bed,:surface,:id_price,:id_view,:id_theme)");
+        $statement->bindValue('name', $room['name'], \PDO::PARAM_STR);
+        $statement->bindValue('description', $room['description'], \PDO::PARAM_STR);
+        $statement->bindValue('nb_bed', $room['nb_bed'], \PDO::PARAM_INT);
+        $statement->bindValue('surface', $room['surface'], \PDO::PARAM_INT);
+        $statement->bindValue('id_price', $room['id_price'], \PDO::PARAM_INT);
+        $statement->bindValue('id_view', $room['id_view'], \PDO::PARAM_INT);
+        $statement->bindValue('id_theme', $room['id_theme'], \PDO::PARAM_INT);
+
+        if ($statement->execute()) {
+            return (int)$this->pdo->lastInsertId();
+        }
+    }
 }
