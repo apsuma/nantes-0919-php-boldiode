4ff8aef1723ad4ba633b5f6b5c5e6f02dbc6b404
Julien Guibert <julien.guib@gmail.com>
2019-10-24 10:00:39 +0200

    US07 modifications about comments

--- src/Model/RoomManager.php
+++ src/Model/RoomManager.php
@@ -23,12 +23,12 @@ class RoomManager extends AbstractManager
 
     public function selectRoomById(int $id)
     {
-        $query = "SELECT r.name room_name, r.description, r.nb_bed, r.surface, r.front_page, ";
-        $query .= "p.price_summer, p.price_winter, p.name price_name, ";
-        $query .= "v.name view_name, t.name theme_name FROM room r INNER JOIN price p ON r.id_price = p.id ";
-        $query .= "INNER JOIN view v ON r.id_view = v.id ";
-        $query .= "INNER JOIN theme t ON r.id_theme = t.id ";
-        $query .= "WHERE r.id = :id";
+        $query = "SELECT r.name room_name, r.description, r.nb_bed, r.surface, r.front_page, 
+        p.price_summer, p.price_winter, p.name price_name, 
+        v.name view_name, t.name theme_name FROM room r INNER JOIN price p ON r.id_price = p.id 
+        INNER JOIN view v ON r.id_view = v.id 
+        INNER JOIN theme t ON r.id_theme = t.id 
+        WHERE r.id = :id";
         $statement = $this->pdo->prepare($query);
         $statement->bindValue('id', $id, \PDO::PARAM_INT);
         $statement->execute();
