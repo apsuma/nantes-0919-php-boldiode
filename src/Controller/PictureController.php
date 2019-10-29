7073ff7dfe0fb6a2ab0957c0bcbb373cdf34ced5
Julien Guibert <julien.guib@gmail.com>
2019-10-29 17:49:57 +0100

    add function delete picture on edit page

---
+++ src/Controller/PictureController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\PictureManager;
+
+class PictureController extends AbstractController
+{
+    public function delete(int $id)
+    {
+        $pictureManager = new PictureManager();
+        $pictureManager->deletePictureId($id);
+        header('location:/room/editList');
+    }
+}
