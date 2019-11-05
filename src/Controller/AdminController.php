58862e2f642bde9e57f386188cb8c47fafb1a3ed
Martin Cazamajor <martin.cazamajor@gmail.com>
2019-11-05 14:58:35 +0100

    verification nom et mdp admin fait

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,10 +2,25 @@
 
 namespace App\Controller;
 
+use App\Model\AdminManager;
+
 class AdminController extends AbstractController
 {
     public function logIn()
     {
         return $this->twig->render("Admin/logIn.html.twig");
+    }
+
+    public function log()
+    {
+        $adminManager = new AdminManager();
+        $admins = $adminManager->selectAll();
+        foreach ($admins as $admin) {
+            if ($_POST['login'] === $admin['login'] && $_POST['pwd'] === $admin['pwd']) {
+                echo "coucou";
+            } else {
+                echo "pas coucou";
+            }
+        }
     }
 }
