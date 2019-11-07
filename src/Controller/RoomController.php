94840eafb09ed928dfdd2394ed00ea88896334d6
Martin Cazamajor <martin.cazamajor@gmail.com>
2019-11-07 11:45:03 +0100

    affichage d'une seule chambre via route et refactor alias des SELECT room query

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -18,18 +18,48 @@ class RoomController extends AbstractController
     public function show() : string
     {
         $roomManager = new RoomManager();
-        $pictureManager = new PictureManager();
         $rooms = $roomManager->selectAllRooms();
-        foreach ($rooms as $key => $room) {
-            $rooms[$key]['images'] = $pictureManager->selectPicturesByRoom($room['id']);
-        }
+        $rooms = $this->selectPicture($rooms);
         return $this->twig->render("Room/show.html.twig", [
             'rooms' => $rooms,
         ]);
     }
 
-    public function search()
+    public function search(int $bed = 0, int $priceId = 0, int $roomId = 0)
     {
-        return $this->show();
+        $roomManager = new RoomManager();
+        if ($roomId != 0) {
+            $room = $roomManager ->selectRoomById($roomId);
+            $room = $this->selectPicture($room);
+            return $this->twig->render("Room/showOneRoom.html.twig", [
+                'room' => $room,
+            ]);
+        } elseif ($bed != 0 && $priceId != 0) {
+            return $this->show();
+        } elseif ($bed != 0) {
+            return $this->show();
+        } elseif ($priceId != 0) {
+            return $this->show();
+        } else {
+            return $this->show();
+        }
+    }
+
+    /**
+     * return an array with the pictures of each room linked to their rooms
+     * @param array $rooms
+     * @return array
+     */
+    public function selectPicture(array $rooms)
+    {
+        $pictureManager = new PictureManager();
+        if (isset($rooms[0])) {
+            foreach ($rooms as $key => $room) {
+                $rooms[$key]['images'] = $pictureManager->selectPicturesByRoom($room['roomId']);
+            }
+        } else {
+            $rooms['images'] = $pictureManager->selectPicturesByRoom($rooms['roomId']);
+        }
+        return $rooms;
     }
 }
