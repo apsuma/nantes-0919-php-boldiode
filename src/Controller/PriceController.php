00880b905b551630dc92712bc25cdb06db970929
Barré <barre.alexandre44@outlook.fr>
2019-11-13 16:56:37 +0100

    road created to editListPrice

---
+++ src/Controller/PriceController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Model\AdminManager;
+use App\Model\FormCheck;
+use App\Model\PriceManager;
+
+class PriceController extends AbstractController
+{
+    public function editListPrice(): string
+    {
+        return $this->twig->render('Admin/editListPrice.html.twig');
+    }
+}
